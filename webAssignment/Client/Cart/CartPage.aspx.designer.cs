fcfe14da26a0c8a0be08bc22d6744ece2571c6ee
Dengwait <tdwei716@gmail.com>
2024-05-01 23:49:35 +0800

    Cart Function Updated

--- webAssignment/Client/Cart/CartPage.aspx.designer.cs
+++ webAssignment/Client/Cart/CartPage.aspx.designer.cs
@@ -112,14 +112,5 @@ namespace webAssignment.Client.Cart
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnProceedCheckout;
-
-        /// <summary>
-        /// lblMessage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblMessage;
     }
 }
