f42f176ec83f877f1908d6b177c498d22af0c141
WeiHong <tanweihong0104@gmail.com>
2024-05-07 08:46:30 +0800

    siok im the 101th

--- webAssignment/Admin/Orders/ViewOrder.aspx.designer.cs
+++ webAssignment/Admin/Orders/ViewOrder.aspx.designer.cs
@@ -114,15 +114,6 @@ namespace webAssignment.Admin.Orders
         protected global::System.Web.UI.WebControls.ListView ordersListView;
 
         /// <summary>
-        /// billingAddlbl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label billingAddlbl;
-
-        /// <summary>
         /// shippingAddresslbl control.
         /// </summary>
         /// <remarks>
